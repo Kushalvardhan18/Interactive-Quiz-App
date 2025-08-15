13a1a79b9d05c04f673b4071db2ac1f6927f2c2e
Kushal Vardhan <vardhank140102@gmail.com>
2025-08-15 20:26:48 +0530

    retry button added and Submit button functionality is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [score, setScore] = useState(0);
   const [hasAnswered, setHasAnswered] = useState(false);
 
+  const [submitted, setSubmitted] = useState(false);
   function handleNextButton() {
     if (count < questions.length - 1) {
       setHasAnswered(false);
@@ -15,7 +16,9 @@ function App() {
     }
   }
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    setSubmitted(true);
+  }
   function verifyResult(option) {
     if (!hasAnswered) {
       if (option === questions[count].correctAnswer) {
@@ -24,29 +27,70 @@ function App() {
     }
     setHasAnswered(true);
   }
+
+  function handleRetry(){
+    setCount(0);
+    setScore(0);
+    setHasAnswered(false);
+    setSubmitted(false)
+  }
   return (
     <div className="flex flex-col  my-5 mx-10">
       <h1 className="font-bold text-6xl !text-amber-300 text-center">Quiz</h1>
-      {/* <h2>Score:{score}</h2> */}
-      <span className="my-8 text-3xl">
-        Question{questions[count].id} : {questions[count].title}
-      </span>
-      <span className="flex flex-col gap-5 ">
-        {questions[count].options.map((option) => (
-          <div>
-            <input
-              type="button"
-              value={option}
-              className=" w-100 border-1 px-5 py-2 rounded-xl hover:!bg-amber-300 hover:!text-yellow-800 text-2xl"
-              onClick={() => verifyResult(option)}
-            />
+      {!submitted ? (
+        <>
+          <div className="my-8 text-3xl">
+            Question {questions[count].id} : {questions[count].title}
           </div>
-        ))}
-      </span>
-      <span className="flex  gap-2 my-5 justify-center">
-        <button onClick={handleNextButton} className="border-1 px-5 py-2 rounded-xl hover:!bg-green-600  w-30">Next</button>
-        <button onClick={handleSubmit} className="border-1 px-5 py-2 rounded-xl hover:!bg-blue-600  w-30">Submit</button>
-      </span>
+          <div className="flex flex-col gap-5 ">
+            {questions[count].options.map((option) => (
+              <div>
+                <input
+                  type="button"
+                  value={option}
+                  className=" w-100 border-1 px-5 py-2 rounded-xl hover:!bg-amber-300 hover:!text-yellow-800 text-2xl"
+                  onClick={() => verifyResult(option)}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex  gap-2 my-5 justify-center">
+            {questions[count].id === 10 ? (
+              <button className="border-1 px-5 py-2 rounded-xl cursor-not-allowed  w-30">
+                Next
+              </button>
+            ) : (
+              <button
+                onClick={handleNextButton}
+                className="border-1 px-5 py-2 rounded-xl hover:!bg-green-600  w-30"
+              >
+                Next
+              </button>
+            )}
+            {questions[count].id === 10 ? (
+              <button
+                onClick={handleSubmit}
+                className="border-1 px-5 py-2 rounded-xl hover:!bg-blue-600   w-30 "
+              >
+                Submit
+              </button>
+            ) : (
+              <button className=" border-1 px-5 py-2 rounded-xl  w-30 cursor-not-allowed">
+                Submit
+              </button>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <button className="flex items-start w-fit border-1 rounded-md border-red-500 px-5 py-2 hover:!bg-amber-400" onClick={handleRetry}>
+            Retry
+          </button>
+          <div class="flex h-100 items-center justify-center">
+            <h2 className="text-6xl ">Your Score : {score}</h2>
+          </div>
+        </>
+      )}
     </div>
   );
 }
